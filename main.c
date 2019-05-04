9e6636ba7183aade9302b3220e4ee2266fe5f431
TaisKha <50263380+TaisKha@users.noreply.github.com>
2019-05-04 13:47:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define HALFSIZE 20
+#define NAME 25
+#define EXTRAS 51
+
+#include "struct.h"
+#include "functions.c"
+
+
+int main(){
+intro();
+menu();
+return 0;
+}
+
