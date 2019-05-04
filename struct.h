e446b55bb6b1d0f305b4e44d4db50b23775f23e2
TaisKha <50263380+TaisKha@users.noreply.github.com>
2019-05-04 13:48:06 +0300

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,1 +1,21 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct
+{
+    int day;
+    int month;
+}date;
+typedef struct hotel
+{
+    char surname[NAME * 2 + 1];
+    char name[NAME * 2 + 1];
+    char patron[NAME * 2 + 1];
+    int room;
+    date dateIN;
+    date dateOUT;
+    int pay;
+    char extra[EXTRAS * 2 + 1];
+} hotel;
+#endif
 
