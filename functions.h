fd601643d6dad249ec40867fccb218e35b9b40a9
TaisKha <50263380+TaisKha@users.noreply.github.com>
2019-05-04 13:48:46 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,25 @@
+#ifndef functions_h
+#define functions_h
+void swapStruct(hotel*, hotel*);
+void printline();
+void printone(hotel*, int);
+void addone(hotel*);
+
+void filling();
+void sort();
+void edit();
+void delete();
+void output();
+void search();
+void addperson();
+
+double getDouble();
+int getInt();
+int dayCheck(int);
+void clean_stdin();
+void intro();
+void menu();
+
+
+#endif
 
